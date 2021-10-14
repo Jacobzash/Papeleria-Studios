e189579f8bfd6c438599e5b258ffd3a648b63196
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 14:54:09 -0500

    Initialize the project

---
+++ src/PapeleriaApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const PapeleriaApp = () => {
+  return (
+    <div className="App">
+      <h1>PapeleriaApp</h1>
+    </div>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { PapeleriaApp } from "./PapeleriaApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PapeleriaApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
