bc11e5e6691794ccb137d7f66150040118abfaf5
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 15:40:55 -0500

    Create Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+export const Login = () => {
+  return (
+    <>
+      <Typography variant="h2" color="initial">
+        Login
+      </Typography>
+    </>
+  );
+};
