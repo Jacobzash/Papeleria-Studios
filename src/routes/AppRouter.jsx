0342c18ede622895c6504b41b35d3130d2747eab
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 17:23:56 -0500

    Use Footer component

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,15 +5,35 @@ import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { ScrollToTop } from "../utils/ScrollToTop";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { Footer } from "../components/Footer/Footer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  main: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    minHeight: "100%",
+  },
+}));
+
 export const AppRouter = () => {
+  const classes = useStyles();
   return (
     <Router>
       <ScrollToTop />
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
+      <div className={classes.root}>
+        <main className={classes.main}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
