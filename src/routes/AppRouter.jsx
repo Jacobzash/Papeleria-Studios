2bfff6663b94362cdcf111f66cf4f9a6f2a1d63c
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 15:42:59 -0500

    Create AppRouter, added Home and Login routes

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
