fef8c3a8002daff6bb77bda684cefe4ba83d8dde
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 15:41:06 -0500

    Create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+export const Home = () => {
+  return (
+    <>
+      <Typography variant="h2" color="initial">
+        Home
+      </Typography>
+    </>
+  );
+};
