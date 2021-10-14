f076ec159a74437e870ccf513f1dadbe03ab7313
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 16:51:11 -0500

    Use ScrollToTop in AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { ScrollToTop } from "../utils/ScrollToTop";
 
 export const AppRouter = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
