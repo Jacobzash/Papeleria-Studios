1640d868a04ecbb232d36caeda2fe450520cba57
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 19:39:34 -0500

    Create basic theme for MUI palette

---
+++ src/themeConfig.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@material-ui/core/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F04A58",
+    },
+    secondary: {
+      main: "#0396A6",
+    },
+    background: {
+      paper: "#1C2541",
+      default: "#F9F9F9",
+    },
+    divider: "#FFFFFF",
+    text: {
+      primary: "#000000",
+    },
+  },
+});
