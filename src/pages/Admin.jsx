e018d6ef5729a7ebc9576f14f028e2dd03a52c05
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-15 10:58:24 -0500

    Create Admin page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export const Admin = () => {
+  return (
+    <>
+      <Typography variant="h2" color="initial">
+        Admin
+      </Typography>
+    </>
+  );
+};
