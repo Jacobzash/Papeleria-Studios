7b1db07959c9839373abe02e62b09f9c51a35107
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-15 10:57:44 -0500

    Remove ReactStrictMode

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { PapeleriaApp } from "./PapeleriaApp";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <PapeleriaApp />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<PapeleriaApp />, document.getElementById("root"));
