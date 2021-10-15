6973a49a00b1dc1144d37295760a5851fa7b11c2
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 19:41:32 -0500

    Move Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Link to="/">
+          <Typography variant="h6" className={classes.title}>
+            Papeleria Studios
+          </Typography>
+        </Link>
+        <Hidden mdUp>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+        <Hidden smDown>
+          <Link to="/login">
+            <Button color="inherit">Login</Button>
+          </Link>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
