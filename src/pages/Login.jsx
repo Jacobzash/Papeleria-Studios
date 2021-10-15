8a932ce85a20d212accf81d321379366c8a047f0
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-14 19:42:50 -0500

    Use FormLogin component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import FormLogin from "../components/FormLogin/FormLogin";
 
 export const Login = () => {
   return (
     <>
-      <Typography variant="h2" color="initial">
-        Login
-      </Typography>
+      <FormLogin />
     </>
   );
 };
