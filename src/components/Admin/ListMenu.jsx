23ffa5cb0f1b4d5aa1cc5abb94850082bb35cbce
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 12:10:51 -0500

    Add Link in ListItems

--- src/components/Admin/ListMenu.jsx
+++ src/components/Admin/ListMenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -12,42 +13,54 @@ import LocalShippingIcon from "@material-ui/icons/LocalShipping";
 export const ListMenu = ({ handleDrawerClose }) => {
   return (
     <div>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <CategoryIcon />
-        </ListItemIcon>
-        <ListItemText primary="Categorías" />
-      </ListItem>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <CreateIcon />
-        </ListItemIcon>
-        <ListItemText primary="Productos" />
-      </ListItem>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <LocalShippingIcon />
-        </ListItemIcon>
-        <ListItemText primary="Proveedores" />
-      </ListItem>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <PlaylistAddCheckIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inventario" />
-      </ListItem>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <AttachMoneyIcon />
-        </ListItemIcon>
-        <ListItemText primary="Ventas" />
-      </ListItem>
-      <ListItem button onClick={handleDrawerClose}>
-        <ListItemIcon>
-          <EqualizerIcon />
-        </ListItemIcon>
-        <ListItemText primary="Estadisticas" />
-      </ListItem>
+      <Link to="/admin/categorias">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <CategoryIcon />
+          </ListItemIcon>
+          <ListItemText primary="Categorías" />
+        </ListItem>
+      </Link>
+      <Link to="/admin/productos">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <CreateIcon />
+          </ListItemIcon>
+          <ListItemText primary="Productos" />
+        </ListItem>
+      </Link>
+      <Link to="/admin/proveedores">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <LocalShippingIcon />
+          </ListItemIcon>
+          <ListItemText primary="Proveedores" />
+        </ListItem>
+      </Link>
+      <Link to="/admin/inventario">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <PlaylistAddCheckIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inventario" />
+        </ListItem>
+      </Link>
+      <Link to="/admin/ventas">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <AttachMoneyIcon />
+          </ListItemIcon>
+          <ListItemText primary="Ventas" />
+        </ListItem>
+      </Link>
+      <Link to="/admin/estadisticas">
+        <ListItem button onClick={handleDrawerClose}>
+          <ListItemIcon>
+            <EqualizerIcon />
+          </ListItemIcon>
+          <ListItemText primary="Estadisticas" />
+        </ListItem>
+      </Link>
     </div>
   );
 };
