0144459c84a64548e2715249e664e01e7e481845
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 11:56:02 -0500

    Create Error404 page

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export const Error404 = () => {
+  return (
+    <div>
+      <Typography variant="h1" color="initial">
+        Error404
+      </Typography>
+    </div>
+  );
+};
