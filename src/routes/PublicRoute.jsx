a556638fda117f03d66acefe25b77337dabdf04e
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 11:56:21 -0500

    Create PublicRoute component

---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Redirect, Route } from "react-router-dom";
+
+export const PublicRoute = ({
+  isLoggedIn,
+  component: Component,
+  restricted,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isLoggedIn && restricted ? (
+          <Redirect to="/admin" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+PublicRoute.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  restricted: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+};
