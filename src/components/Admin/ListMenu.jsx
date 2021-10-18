6d805ab8be8f5cc592f947864b6768b81090f5ab
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 10:32:49 -0500

    Create ListMenu component for admin

---
+++ src/components/Admin/ListMenu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import CategoryIcon from "@material-ui/icons/Category";
+import CreateIcon from "@material-ui/icons/Create";
+import PlaylistAddCheckIcon from "@material-ui/icons/PlaylistAddCheck";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+
+export const ListMenu = ({ handleDrawerClose }) => {
+  return (
+    <div>
+      <ListItem button onClick={handleDrawerClose}>
+        <ListItemIcon>
+          <CategoryIcon />
+        </ListItemIcon>
+        <ListItemText primary="Categorías" />
+      </ListItem>
+      <ListItem button onClick={handleDrawerClose}>
+        <ListItemIcon>
+          <CreateIcon />
+        </ListItemIcon>
+        <ListItemText primary="Productos" />
+      </ListItem>
+      <ListItem button onClick={handleDrawerClose}>
+        <ListItemIcon>
+          <PlaylistAddCheckIcon />
+        </ListItemIcon>
+        <ListItemText primary="Inventario" />
+      </ListItem>
+      <ListItem button onClick={handleDrawerClose}>
+        <ListItemIcon>
+          <AttachMoneyIcon />
+        </ListItemIcon>
+        <ListItemText primary="Ventas" />
+      </ListItem>
+      <ListItem button onClick={handleDrawerClose}>
+        <ListItemIcon>
+          <EqualizerIcon />
+        </ListItemIcon>
+        <ListItemText primary="Estadisticas" />
+      </ListItem>
+    </div>
+  );
+};
