2c42b93c6b847d3a42d5444ad7aee367712e336d
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 16:51:59 -0500

    Create ButtonDeleteCategoria component

---
+++ src/components/Categorias/ButtonDeleteCategoria.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+export const ButtonDeleteCategoria = () => {
+  return (
+    <IconButton color="secondary">
+      <DeleteForeverIcon />
+    </IconButton>
+  );
+};
