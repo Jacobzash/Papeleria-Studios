c921449eccf1f24afe2ae9e1ec2869151cb18f19
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 16:50:46 -0500

    Update imports and use of Categorias component

--- src/routes/AdminRouter.jsx
+++ src/routes/AdminRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Categoria } from "../pages/Categoria";
+import { Categorias } from "../pages/Categorias";
 import { Admin } from "../pages/Admin";
 import { Error404 } from "../pages/Error404";
 export const AdminRouter = () => {
@@ -8,12 +8,12 @@ export const AdminRouter = () => {
     <>
       <Switch>
         <Route exact path="/admin" component={Admin} />
-        <Route exact path="/admin/categorias" component={Categoria} />
-        <Route exact path="/admin/productos" component={Categoria} />
-        <Route exact path="/admin/proveedores" component={Categoria} />
-        <Route exact path="/admin/inventario" component={Categoria} />
-        <Route exact path="/admin/ventas" component={Categoria} />
-        <Route exact path="/admin/estadisticas" component={Categoria} />
+        <Route exact path="/admin/categorias" component={Categorias} />
+        <Route exact path="/admin/productos" component={Categorias} />
+        <Route exact path="/admin/proveedores" component={Categorias} />
+        <Route exact path="/admin/inventario" component={Categorias} />
+        <Route exact path="/admin/ventas" component={Categorias} />
+        <Route exact path="/admin/estadisticas" component={Categorias} />
         <Route component={Error404} />
       </Switch>
     </>
