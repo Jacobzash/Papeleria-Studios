cec3bed4e8a8a187b01fd43c357cc9092b117925
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-18 19:09:45 -0500

    Added function openModal

--- src/components/Categorias/AddCategoria.jsx
+++ src/components/Categorias/AddCategoria.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
+import { ModalCategorias } from "./ModalCategorias";
 
 export const AddCategoria = () => {
+  const [open, setOpen] = useState(false);
+  const openModal = () => {
+    setOpen(true);
+  };
   return (
-    <Button 
-    variant="contained" 
-    color="primary" 
-    fullWidth 
-    endIcon={<AddIcon />}
-    >
-      Agregar
-    </Button>
+    <>
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        endIcon={<AddIcon />}
+        onClick={openModal}
+      >
+        Agregar
+      </Button>
+      <ModalCategorias open={open} setOpen={setOpen} />
+    </>
   );
 };
