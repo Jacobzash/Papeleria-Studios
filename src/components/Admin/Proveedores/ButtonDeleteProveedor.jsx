22e9d6f7c8bb3fb3a0f5e65122a694e7a9e8c993
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-21 11:58:36 -0500

    Create actions button for TableProveedores component

---
+++ src/components/Admin/Proveedores/ButtonDeleteProveedor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+export const ButtonDeleteProveedor = () => {
+  return (
+    <IconButton color="secondary">
+      <DeleteForeverIcon />
+    </IconButton>
+  );
+};
