c241e692b1eb6d2f5ed6a2fd770eb46b6478906e
Marlon Gómez <marlonjulian15@gmail.com>
2021-10-22 12:33:16 -0500

    Added minWidth for Modal

--- src/components/Admin/Productos/ModalProducto.jsx
+++ src/components/Admin/Productos/ModalProducto.jsx
@@ -10,6 +10,14 @@ import CloseIcon from "@material-ui/icons/Close";
 import { FormProducto } from "./FormProducto";
 
 const useStyles = makeStyles((theme) => ({
+  dialog: {
+    [theme.breakpoints.between(420, 600)]: {
+      minWidth: 360,
+    },
+    [theme.breakpoints.up("sm")]: {
+      minWidth: 550,
+    },
+  },
   headerDialog: {
     display: "flex",
     alignItems: "center",
@@ -36,43 +44,45 @@ export const ModalProducto = ({ open, setOpen, mode, data }) => {
         open={open}
         scroll="paper"
       >
-        <div className={classes.headerDialog}>
-          <DialogTitle id="Producto" onClose={handleClose}>
-            {mode === "edit"
-              ? `Editando el producto ${data.nombre}`
-              : "Crear un producto"}
-          </DialogTitle>
-          <IconButton aria-label="Cerrar modal" onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
+        <div className={classes.dialog}>
+          <div className={classes.headerDialog}>
+            <DialogTitle id="Producto" onClose={handleClose}>
+              {mode === "edit"
+                ? `Editando el producto ${data.nombre}`
+                : "Crear un producto"}
+            </DialogTitle>
+            <IconButton aria-label="Cerrar modal" onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <DialogContent dividers>
+            <FormProducto
+              dataProducto={dataProducto}
+              setDataProducto={setDataProducto}
+              handleSubmit={handleSubmit}
+              data={data}
+              mode={mode}
+            />
+          </DialogContent>
+          <DialogActions className={classes.actions}>
+            <Button
+              autoFocus
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+            >
+              Guardar producto
+            </Button>
+            <Button
+              autoFocus
+              onClick={handleClose}
+              variant="contained"
+              color="secondary"
+            >
+              Cancelar
+            </Button>
+          </DialogActions>
         </div>
-        <DialogContent dividers>
-          <FormProducto
-            dataProducto={dataProducto}
-            setDataProducto={setDataProducto}
-            handleSubmit={handleSubmit}
-            data={data}
-            mode={mode}
-          />
-        </DialogContent>
-        <DialogActions className={classes.actions}>
-          <Button
-            autoFocus
-            onClick={handleSubmit}
-            variant="contained"
-            color="primary"
-          >
-            Guardar producto
-          </Button>
-          <Button
-            autoFocus
-            onClick={handleClose}
-            variant="contained"
-            color="secondary"
-          >
-            Cancelar
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
