e4239144abe939e7d8455fadea8210b73698f212
Marlon Gómez <marlonjulian15@gmail.com>
2021-11-26 10:32:54 -0500

    Use AuthContext

--- src/PapeleriaApp.jsx
+++ src/PapeleriaApp.jsx
@@ -3,11 +3,14 @@ import { AppRouter } from "./routes/AppRouter";
 
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./themeConfig";
+import { AuthProvider } from "./context/AuthContext";
 
 export const PapeleriaApp = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <AppRouter />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
