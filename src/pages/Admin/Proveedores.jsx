671c0b0d3f010cd5f0397991db476d0e14adaaf7
Marlon Gómez <marlonjulian15@gmail.com>
2021-11-27 13:13:56 -0500

    Load providers

--- src/pages/Admin/Proveedores.jsx
+++ src/pages/Admin/Proveedores.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { InputSearchProveedor } from "../../components/Admin/Proveedores/InputSearchProveedor";
 import { TableProveedores } from "../../components/Admin/Proveedores/TableProveedores";
+import { getProvidersApi } from "../../api/provider";
+import { Loading } from "../../components/Others/Loading";
+
+import { ProvidersContext } from "../../context/ProvidersContext";
 
 export const Proveedores = () => {
+  const { providers, setProviders } = useContext(ProvidersContext);
+  console.log(providers);
+  useEffect(() => {
+    const getProveedores = async () => {
+      const response = await getProvidersApi();
+      if (response.ok) {
+        setProviders(response.providers);
+      }
+    };
+    getProveedores();
+  }, [setProviders]);
+
   return (
     <>
       <Typography variant="h3" color="primary" align="center">
         Módulo de Proveedores
       </Typography>
       <Container component="div" maxWidth="lg">
-        <InputSearchProveedor />
-        <TableProveedores />
+        {!providers ? (
+          <Loading variantMessage="h5" message="Cargando proveedores..." />
+        ) : (
+          <>
+            <InputSearchProveedor />
+            <TableProveedores />
+          </>
+        )}
       </Container>
     </>
   );
