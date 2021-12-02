a5a2840848bd617a4f5a05dcd1fa3f7d97446826
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 13:38:05 -0500

    Create CategoriesContext

---
+++ src/context/CategoriesContext.jsx
@@ -0,0 +1,13 @@
+import React, { useState, createContext } from "react";
+
+export const CategoriesContext = createContext();
+
+export const CategoriesProvider = ({ children }) => {
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <CategoriesContext.Provider value={{ categories, setCategories }}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
