850d15c6c8311fbc4c49402fe33dd941feffd193
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 13:38:28 -0500

    Use CategoriesContext

--- src/routes/AdminRouter.jsx
+++ src/routes/AdminRouter.jsx
@@ -6,13 +6,16 @@ import { Error404 } from "../pages/Error404";
 import { Proveedores } from "../pages/Admin/Proveedores";
 import { Productos } from "../pages/Admin/Productos";
 import { ProvidersProvider } from "../context/ProvidersContext";
+import { CategoriesProvider } from "../context/CategoriesContext";
 
 export const AdminRouter = () => {
   return (
     <>
       <Switch>
         <Route exact path="/admin" component={Admin} />
-        <Route exact path="/admin/categorias" component={Categorias} />
+        <CategoriesProvider>
+          <Route exact path="/admin/categorias" component={Categorias} />
+        </CategoriesProvider>
         <Route exact path="/admin/productos" component={Productos} />
         <ProvidersProvider>
           <Route exact path="/admin/proveedores" component={Proveedores} />
