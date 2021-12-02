e8eeb2ad5641f65ee466919f89639865b5a90767
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 13:47:20 -0500

    Load categories

--- src/pages/Admin/Categorias.jsx
+++ src/pages/Admin/Categorias.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 import { InputSearchCategoria } from "../../components/Admin/Categorias/InputSearchCategoria";
 import { TableCategorias } from "../../components/Admin/Categorias/TableCategorias";
 
+import { CategoriesContext } from "../../context/CategoriesContext";
+import { getCategoriesApi } from "../../api/category";
+import { Loading } from "../../components/Others/Loading";
+
 export const Categorias = () => {
+  const { categories, setCategories } = useContext(CategoriesContext);
+
+  useEffect(() => {
+    const getCategorias = async () => {
+      const response = await getCategoriesApi();
+      if (response.ok) {
+        setCategories(response.categories);
+      }
+    };
+    getCategorias();
+  }, []);
+
   return (
     <>
       <Typography variant="h3" color="primary" align="center">
         Módulo de Categorías
       </Typography>
       <Container component="div" maxWidth="lg">
-        <InputSearchCategoria />
-        <TableCategorias />
+        {!categories ? (
+          <Loading variantMessage="h5" message="Cargando categorias..." />
+        ) : (
+          <>
+            <InputSearchCategoria />
+            <TableCategorias />
+          </>
+        )}
       </Container>
     </>
   );
