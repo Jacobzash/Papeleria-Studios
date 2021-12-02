30d09f7785ea105b16a929da4ba811bde65190ef
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 14:04:43 -0500

    Use DeleteCategoryApi

--- src/components/Admin/Categorias/ButtonDeleteCategoria.jsx
+++ src/components/Admin/Categorias/ButtonDeleteCategoria.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { deleteCategoryApi } from "../../../api/category";
+import Swal from "sweetalert2";
 
-export const ButtonDeleteCategoria = () => {
+import { CategoriesContext } from "../../../context/CategoriesContext";
+
+export const ButtonDeleteCategoria = ({ data }) => {
+  const { categories, setCategories } = useContext(CategoriesContext);
+  const handleClick = async () => {
+    const result = await Swal.fire({
+      title: `¿Está seguro de borrar la categoria ${data.nom_cat}?`,
+      text: "No se puede revertir esta acción.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, borrar esto",
+      cancelButtonText: "Cancelar",
+    });
+    if (result.isConfirmed) {
+      const response = await deleteCategoryApi(data.id);
+      if (response.ok) {
+        Swal.fire(response.msg, "", "success");
+        setCategories(categories.filter((category) => category.id !== data.id));
+      }
+    }
+  };
   return (
-    <IconButton color="secondary">
+    <IconButton color="secondary" onClick={handleClick}>
       <DeleteForeverIcon />
     </IconButton>
   );
