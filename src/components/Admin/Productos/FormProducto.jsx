2a1cc730078b068b2f8ac43acf3b5ce531e89002
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-03 11:45:08 -0500

    BugFix of States

--- src/components/Admin/Productos/FormProducto.jsx
+++ src/components/Admin/Productos/FormProducto.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import MenuItem from "@material-ui/core/MenuItem";
+import Typography from "@material-ui/core/Typography";
 import Select from "@material-ui/core/Select";
 import Grid from "@material-ui/core/Grid";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { AddCategoria } from "../Categorias/AddCategoria";
 import { AddProveedor } from "../Proveedores/AddProveedor";
+import { AdminContext } from "../../../context/AdminContext";
 
 const useStyles = makeStyles((theme) => ({
   displayNone: {
@@ -20,11 +21,13 @@ const useStyles = makeStyles((theme) => ({
 export const FormProducto = ({
   dataProducto,
   setDataProducto,
-  handleSubmit,
-  data,
+  register,
+  errors,
   mode,
 }) => {
   const classes = useStyles();
+  const { providers, categories } = useContext(AdminContext);
+
   const handleChange = ({ target }) => {
     setDataProducto({
       ...dataProducto,
@@ -37,124 +40,171 @@ export const FormProducto = ({
       console.log(file);
     }
   };
+
   const matches = useMediaQuery("(min-width:600px)");
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container direction="column">
-        <Grid item xs={12}>
-          <TextField
-            autoFocus
-            color="secondary"
-            fullWidth
-            label="Nombre"
-            margin="normal"
-            onChange={handleChange}
-            defaultValue={mode === "edit" ? dataProducto.nombre : ""}
-            name="nombre"
-            required
-            variant="outlined"
-          />
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={matches ? 1 : 0}
-        >
-          <Grid item xs={12} sm={10}>
-            <FormControl fullWidth variant="outlined" margin="normal">
-              <InputLabel id="select-categoria">Categoria</InputLabel>
-              <Select
-                labelId="select-categoria"
-                id="select-categoria"
-                name="categoria"
-                label="Categoria"
-                value={mode === "edit" ? dataProducto.categoria : ""}
-                onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>Seleccione una categoria</em>
-                </MenuItem>
-                <MenuItem value="Lapices">Lapices</MenuItem>
-                <MenuItem value="Borradores">Borradores</MenuItem>
-                <MenuItem value="Correctores">Correctores</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <AddCategoria mode="icon" />
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={matches ? 1 : 0}
-        >
-          <Grid item xs={12} sm={10}>
-            <FormControl fullWidth variant="outlined" margin="normal">
-              <InputLabel id="select-proveedor">Proveedor</InputLabel>
-              <Select
-                labelId="select-proveedor"
-                id="select-proveedor"
-                name="proveedor"
-                label="Proveedor"
-                value={mode === "edit" ? dataProducto.proveedor : ""}
-                onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>Seleccione una categoria</em>
-                </MenuItem>
-                <MenuItem value="Norma">Norma</MenuItem>
-                <MenuItem value="Tulip">Tulip</MenuItem>
-                <MenuItem value="Astro">Astro</MenuItem>
-                <MenuItem value="Deno">Deno</MenuItem>
-                <MenuItem value="Hello">Hello</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <AddProveedor mode="icon" />
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            color="secondary"
-            fullWidth
-            label="Precio"
-            margin="normal"
-            onChange={handleChange}
-            defaultValue={mode === "edit" ? dataProducto.precio : ""}
-            name="precio"
-            type="number"
-            required
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <input
-            accept="image/*"
-            className={classes.displayNone}
-            id="contained-button-file"
-            name="imgProducto"
-            onChange={handleFileChange}
-            type="file"
-          />
-          <label htmlFor="contained-button-file">
-            <Button
-              variant="contained"
-              fullWidth
-              color="primary"
-              component="span"
-            >
-              Subir Imagen
-            </Button>
-          </label>
-        </Grid>
+    <Grid container direction="column">
+      <Grid item xs={12}>
+        <TextField
+          autoFocus
+          color="secondary"
+          fullWidth
+          label="Nombre"
+          margin="normal"
+          onChange={handleChange}
+          defaultValue={mode === "edit" ? dataProducto.nom_produc : ""}
+          name="nom_produc"
+          variant="outlined"
+          inputRef={register({
+            required: {
+              value: true,
+              message: "El nombre del producto es requerido",
+            },
+          })}
+        />
+        <Typography variant="body1" display="block" color="error" gutterBottom>
+          {errors?.nom_produc?.message}
+        </Typography>
       </Grid>
-    </form>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={matches ? 1 : 0}
+      >
+        <Grid item xs={12} sm={10}>
+          <FormControl fullWidth variant="outlined" margin="normal">
+            <InputLabel id="select-categoria">Categoria</InputLabel>
+            <Select
+              native
+              labelId="select-categoria"
+              id="select-categoria"
+              name="id_categoria"
+              label="Categoria"
+              defaultValue={mode === "edit" ? dataProducto?.Categoria?.id : ""}
+              onChange={handleChange}
+              inputRef={register({
+                required: {
+                  value: true,
+                  message: "La categoría es requerida",
+                },
+              })}
+            >
+              <option aria-label="none" value="" />
+              {categories.map((category) => {
+                return (
+                  <option key={category.id} value={category.id}>
+                    {category.nom_cat}
+                  </option>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <AddCategoria mode="icon" />
+        </Grid>
+        <Typography variant="body1" display="block" color="error" gutterBottom>
+          {errors?.id_categoria?.message}
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={matches ? 1 : 0}
+      >
+        <Grid item xs={12} sm={10}>
+          <FormControl fullWidth variant="outlined" margin="normal">
+            <InputLabel id="select-proveedor">Proveedor</InputLabel>
+            <Select
+              native
+              labelId="select-proveedor"
+              id="select-proveedor"
+              name="id_proveedor"
+              label="Proveedor"
+              defaultValue={mode === "edit" ? dataProducto?.Proveedor?.id : ""}
+              onChange={handleChange}
+              inputRef={register({
+                required: {
+                  value: true,
+                  message: "El proveedor es requerido",
+                },
+              })}
+            >
+              <option aria-label="none" value="" />
+              {providers.map((provider) => {
+                return (
+                  <option key={provider.id} value={provider.id}>
+                    {provider.nom_prov}
+                  </option>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <AddProveedor mode="icon" />
+        </Grid>
+        <Typography variant="body1" display="block" color="error" gutterBottom>
+          {errors?.id_proveedor?.message}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          color="secondary"
+          fullWidth
+          label="Precio"
+          margin="normal"
+          onChange={handleChange}
+          defaultValue={mode === "edit" ? dataProducto.valor_unitario : ""}
+          name="valor_unitario"
+          type="number"
+          variant="outlined"
+          inputRef={register({
+            required: {
+              value: true,
+              message: "El precio del producto es requerido",
+            },
+          })}
+        />
+        <Typography variant="body1" display="block" color="error" gutterBottom>
+          {errors?.valor_unitario?.message}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <input
+          accept="image/*"
+          className={classes.displayNone}
+          id="contained-button-file"
+          name="url_image"
+          onChange={handleFileChange}
+          type="file"
+          ref={register({
+            required: {
+              value: true,
+              message: "La imagen del producto es requerida",
+            },
+          })}
+        />
+
+        <label htmlFor="contained-button-file">
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            component="span"
+          >
+            Subir Imagen
+          </Button>
+        </label>
+        <Typography variant="body1" display="block" color="error" gutterBottom>
+          {errors?.url_image?.message}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
--- src/components/Admin/Productos/ModalProducto.jsx
+++ src/components/Admin/Productos/ModalProducto.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
@@ -8,6 +8,13 @@ import DialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import { FormProducto } from "./FormProducto";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { createProductApi, updateProductApi } from "../../../api/product";
+import { AdminContext } from "../../../context/AdminContext";
+import { getCategoriesApi } from "../../../api/category";
+import { getProvidersApi } from "../../../api/provider";
+import { Loading } from "../../Others/Loading";
 
 const useStyles = makeStyles((theme) => ({
   dialog: {
@@ -27,63 +34,111 @@ const useStyles = makeStyles((theme) => ({
 
 export const ModalProducto = ({ open, setOpen, mode, data }) => {
   const [dataProducto, setDataProducto] = useState(data);
+  const { products, setProducts } = useContext(AdminContext);
+  const { register, handleSubmit, errors } = useForm();
   const classes = useStyles();
+
+  const { setProviders, setCategories, categories } = useContext(AdminContext);
+
+  useEffect(() => {
+    const getDataSelects = async () => {
+      const resultProviders = await getProvidersApi();
+      const resultCategories = await getCategoriesApi();
+      if (resultProviders.ok) {
+        setProviders(resultProviders.providers);
+      }
+      if (resultCategories.ok) {
+        setCategories(resultCategories.categories);
+      }
+    };
+    getDataSelects();
+  }, [setProviders, setCategories]);
+
   const handleClose = () => {
     setOpen(false);
   };
-  const handleSubmit = (e) => {
+
+  const onSubmit = async (_, e) => {
     e.preventDefault();
-    console.log(dataProducto);
+    if (mode === "edit") {
+      const result = await updateProductApi(dataProducto);
+      if (result.ok) {
+        handleClose();
+        setProducts(
+          products.map((product) => {
+            if (product.id === result.product.id) {
+              return result.product;
+            }
+            return product;
+          })
+        );
+        Swal.fire(result.msg, "", "success");
+      }
+    } else {
+      const result = await createProductApi(dataProducto);
+      if (result.ok) {
+        handleClose();
+        const product = result.product;
+        setProducts([product, ...products]);
+        Swal.fire(result.msg, "", "success");
+      }
+    }
     handleClose();
   };
   return (
     <div>
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="Modal de Productos"
-        open={open}
-        scroll="paper"
-      >
-        <div className={classes.dialog}>
-          <div className={classes.headerDialog}>
-            <DialogTitle id="Producto" onClose={handleClose}>
-              {mode === "edit"
-                ? `Editando el producto ${data.nombre}`
-                : "Crear un producto"}
-            </DialogTitle>
-            <IconButton aria-label="Cerrar modal" onClick={handleClose}>
-              <CloseIcon />
-            </IconButton>
+      {!categories ? (
+        <Loading />
+      ) : (
+        <Dialog
+          onClose={handleClose}
+          aria-labelledby="Modal de Productos"
+          open={open}
+          scroll="paper"
+        >
+          <div className={classes.dialog}>
+            <div className={classes.headerDialog}>
+              <DialogTitle id="Producto" onClose={handleClose}>
+                {mode === "edit"
+                  ? `Editando el producto ${data.nombre}`
+                  : "Crear un producto"}
+              </DialogTitle>
+              <IconButton aria-label="Cerrar modal" onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <DialogContent dividers>
+                <FormProducto
+                  dataProducto={dataProducto}
+                  setDataProducto={setDataProducto}
+                  mode={mode}
+                  register={register}
+                  errors={errors}
+                />
+              </DialogContent>
+              <DialogActions className={classes.actions}>
+                <Button
+                  autoFocus
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                >
+                  Guardar producto
+                </Button>
+                <Button
+                  autoFocus
+                  onClick={handleClose}
+                  variant="contained"
+                  color="secondary"
+                >
+                  Cancelar
+                </Button>
+              </DialogActions>
+            </form>
           </div>
-          <DialogContent dividers>
-            <FormProducto
-              dataProducto={dataProducto}
-              setDataProducto={setDataProducto}
-              handleSubmit={handleSubmit}
-              data={data}
-              mode={mode}
-            />
-          </DialogContent>
-          <DialogActions className={classes.actions}>
-            <Button
-              autoFocus
-              onClick={handleSubmit}
-              variant="contained"
-              color="primary"
-            >
-              Guardar producto
-            </Button>
-            <Button
-              autoFocus
-              onClick={handleClose}
-              variant="contained"
-              color="secondary"
-            >
-              Cancelar
-            </Button>
-          </DialogActions>
-        </div>
-      </Dialog>
+        </Dialog>
+      )}
     </div>
   );
 };
