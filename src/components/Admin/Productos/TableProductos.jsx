b52eb434f30dd7c10421112f9dce48b8614fb0f1
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 21:07:24 -0500

    Load products in TableProductos

--- src/components/Admin/Productos/TableProductos.jsx
+++ src/components/Admin/Productos/TableProductos.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -9,6 +9,8 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { ButtonEditProducto } from "./ButtonEditProducto";
 import { ButtonDeleteProducto } from "./ButtonDeleteProducto";
+
+import { AdminContext } from "../../../context/AdminContext";
 
 const useStyles = makeStyles((theme) => ({
   tableContainer: {
@@ -29,17 +31,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function createData(ID, nombre, categoria, proveedor, precio) {
-  return { ID, nombre, categoria, proveedor, precio };
-}
-const rows = [
-  createData(154, "Lapiz Norma", "Lapices", "Norma", 500),
-  createData(149, "Corrector Astro", "Correctores", "Astro", 1500),
-  createData(129, "Borrador de Nata", "Borradores", "Norma", 800),
-  createData(139, "Lapicero Norma", "Lapices", "Norma", 900),
-];
 export const TableProductos = () => {
   const classes = useStyles();
+  const { products } = useContext(AdminContext);
 
   return (
     <TableContainer className={classes.tableContainer} component={Paper}>
@@ -73,20 +67,20 @@ export const TableProductos = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow className={classes.tableRow} key={row.ID}>
+          {products.map((product) => (
+            <TableRow className={classes.tableRow} key={product.id}>
               <TableCell component="th" scope="row">
-                {row.ID}
+                {product.id}
               </TableCell>
-              <TableCell align="left">{row.nombre}</TableCell>
-              <TableCell align="left">{row.categoria}</TableCell>
-              <TableCell align="left">{row.proveedor}</TableCell>
-              <TableCell align="left">{"$" + row.precio}</TableCell>
+              <TableCell align="left">{product.nom_produc}</TableCell>
+              <TableCell align="left">{product.Categoria.nom_cat}</TableCell>
+              <TableCell align="left">{product.Proveedor.nom_prov}</TableCell>
+              <TableCell align="left">{"$" + product.valor_unitario}</TableCell>
               <TableCell align="left">
-                <ButtonEditProducto data={row} />
+                <ButtonEditProducto data={product} />
               </TableCell>
               <TableCell align="left">
-                <ButtonDeleteProducto />
+                <ButtonDeleteProducto data={product} />
               </TableCell>
             </TableRow>
           ))}
