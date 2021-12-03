6d69e4daf2d3b05d22df1a98cf17e6cad0a750ee
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 20:22:16 -0500

    Create and use getProductsApi

--- src/pages/Admin/Productos.jsx
+++ src/pages/Admin/Productos.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { InputSearchProducto } from "../../components/Admin/Productos/InputSearchProducto";
 import { TableProductos } from "../../components/Admin/Productos/TableProductos";
 
+import { AdminContext } from "../../context/AdminContext";
+import { Loading } from "../../components/Others/Loading";
+import { getProductsApi } from "../../api/product";
+
 export const Productos = () => {
+  const { products, setProducts } = useContext(AdminContext);
+  useEffect(() => {
+    const getProductos = async () => {
+      const response = await getProductsApi();
+      if (response.ok) {
+        setProducts(response.products);
+      }
+    };
+    getProductos();
+  }, [setProducts]);
   return (
     <>
       <Typography variant="h3" color="primary" align="center">
         Módulo de Productos
       </Typography>
       <Container component="div" maxWidth="lg">
-        <InputSearchProducto />
-        <TableProductos />
+        {!products ? (
+          <Loading variantMessage="h5" message="Cargando productos..." />
+        ) : (
+          <>
+            <InputSearchProducto />
+            <TableProductos />
+          </>
+        )}
       </Container>
     </>
   );
