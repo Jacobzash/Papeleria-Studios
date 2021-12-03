138e8b15d792acac8ff7e80bd045464f3babfff7
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 21:19:24 -0500

    Create and Use DeleteProductApi

--- src/components/Admin/Productos/ButtonDeleteProducto.jsx
+++ src/components/Admin/Productos/ButtonDeleteProducto.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import Swal from "sweetalert2";
 
-export const ButtonDeleteProducto = () => {
+import { AdminContext } from "../../../context/AdminContext";
+import { deleteProductApi } from "../../../api/product";
+
+export const ButtonDeleteProducto = ({ data }) => {
+  const { products, setProducts } = useContext(AdminContext);
+
+  const handleClick = async () => {
+    const result = await Swal.fire({
+      title: `¿Está seguro de borrar el producto ${data.nom_produc}?`,
+      text: "No se puede revertir esta acción.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, borrar esto",
+      cancelButtonText: "Cancelar",
+    });
+    if (result.isConfirmed) {
+      const response = await deleteProductApi(data.id);
+      if (response.ok) {
+        Swal.fire(response.msg, "", "success");
+        setProducts(products.filter((product) => product.id !== data.id));
+      }
+    }
+  };
   return (
-    <IconButton color="secondary">
+    <IconButton color="secondary" onClick={handleClick}>
       <DeleteForeverIcon />
     </IconButton>
   );
