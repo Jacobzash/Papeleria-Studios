1bbe463fb70c9eefabafbf21f80a79deb98eac0d
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-03 11:45:28 -0500

    Added Update and Create Product API

--- src/api/product.js
+++ src/api/product.js
@@ -22,7 +22,68 @@ export const getProductsApi = async () => {
     }
   }
 };
+export const createProductApi = async ({
+  nom_produc: nombre,
+  valor_unitario: precio,
+  url_image,
+  id_categoria,
+  id_proveedor,
+}) => {
+  const url = `${urlBase}`;
+  const token = getToken();
+  const headers = {
+    "Content-Type": "application/json",
+    "x-token": token,
+  };
+  url_image =
+    "https://larepublica.pe/resizer/csNa2bb_rRBUbbmKPp2Hbtd_nSM=/660x360/top/larepublica.pe/resizer/8ZuQ7BjJnugC7n3W0FStVtmDCQU=/660x360/top/smart/larepublica.pe/resizer/aE-G5N9mYRbYweeSPO9nIiEcAm0=/660x360/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/ZGCXB332FFEGLLWMZJQSCW5KHM.jpg";
+  try {
+    const response = await axios.post(
+      url,
+      { nombre, precio, url_image, id_categoria, id_proveedor },
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      return error.message;
+    }
+  }
+};
 
+export const updateProductApi = async ({
+  id,
+  nom_produc: nombre,
+  valor_unitario: precio,
+  url_image,
+  id_categoria,
+  id_proveedor,
+}) => {
+  const url = `${urlBase}`;
+  const token = getToken();
+  const headers = {
+    "Content-Type": "application/json",
+    "x-token": token,
+  };
+  url_image =
+    "https://larepublica.pe/resizer/csNa2bb_rRBUbbmKPp2Hbtd_nSM=/660x360/top/larepublica.pe/resizer/8ZuQ7BjJnugC7n3W0FStVtmDCQU=/660x360/top/smart/larepublica.pe/resizer/aE-G5N9mYRbYweeSPO9nIiEcAm0=/660x360/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/ZGCXB332FFEGLLWMZJQSCW5KHM.jpg";
+  try {
+    const response = await axios.put(
+      url,
+      { id, nombre, precio, url_image, id_categoria, id_proveedor },
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      return error.message;
+    }
+  }
+};
 export const deleteProductApi = async (id) => {
   const url = `${urlBase}`;
   const token = getToken();
