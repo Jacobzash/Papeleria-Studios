697ce599be4e1902367c679ae8f4c2f2b49bfbce
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-02 20:21:08 -0500

    Added products state

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -5,10 +5,18 @@ export const AdminContext = createContext();
 export const AdminProvider = ({ children }) => {
   const [providers, setProviders] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
 
   return (
     <AdminContext.Provider
-      value={{ providers, categories, setProviders, setCategories }}
+      value={{
+        providers,
+        categories,
+        setProviders,
+        setCategories,
+        products,
+        setProducts,
+      }}
     >
       {children}
     </AdminContext.Provider>
