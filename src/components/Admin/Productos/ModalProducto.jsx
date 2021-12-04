6c8ca5f2696e8df8cdfc195f271b3ebc2839fd01
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-03 21:13:20 -0500

    Delete warnings

--- src/components/Admin/Productos/ModalProducto.jsx
+++ src/components/Admin/Productos/ModalProducto.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
