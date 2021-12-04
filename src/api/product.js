da97529f8de67ed9a60608a6036d167e06530d88
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-03 19:48:01 -0500

    Added uploadImageApi

--- src/api/product.js
+++ src/api/product.js
@@ -35,8 +35,13 @@ export const createProductApi = async ({
     "Content-Type": "application/json",
     "x-token": token,
   };
-  url_image =
-    "https://larepublica.pe/resizer/csNa2bb_rRBUbbmKPp2Hbtd_nSM=/660x360/top/larepublica.pe/resizer/8ZuQ7BjJnugC7n3W0FStVtmDCQU=/660x360/top/smart/larepublica.pe/resizer/aE-G5N9mYRbYweeSPO9nIiEcAm0=/660x360/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/ZGCXB332FFEGLLWMZJQSCW5KHM.jpg";
+  url_image = await uploadImageProductApi(url_image);
+  if (url_image === null) {
+    return {
+      ok: false,
+      msg: "No se pudo subir la imagen",
+    };
+  }
   try {
     const response = await axios.post(
       url,
@@ -103,5 +108,24 @@ export const deleteProductApi = async (id) => {
     } else {
       return error.message;
     }
+  }
+};
+
+const uploadImageProductApi = async (file) => {
+  const cloudUrl = "https://api.cloudinary.com/v1_1/dnnahcrob/image/upload";
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "papeleria-studios");
+
+  try {
+    const resp = await axios.post(cloudUrl, formData);
+    if (resp.status === 200) {
+      return resp.data.secure_url;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
