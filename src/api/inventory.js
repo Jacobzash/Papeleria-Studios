2c2dbbffb80d455501d51f3492464ba5360ed138
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-09 08:40:13 -0500

    Create inventory API

---
+++ src/api/inventory.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import getToken from "../utils/getToken";
+import { apiVersion, basePath } from "./config";
+
+const urlBase = `${basePath}/${apiVersion}/inventory`;
+
+export const getInventoryApi = async () => {
+  const url = `${urlBase}`;
+  const token = getToken();
+  const headers = {
+    "Content-Type": "application/json",
+    "x-token": token,
+  };
+  try {
+    const response = await axios.get(url, { headers });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      return error.message;
+    }
+  }
+};
