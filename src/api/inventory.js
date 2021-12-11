7bfc2a4170a2b9066e656224c617faaa62d165e3
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-10 21:08:23 -0500

    Added getInventoryWithStockApi

--- src/api/inventory.js
+++ src/api/inventory.js
@@ -22,6 +22,25 @@ export const getInventoryApi = async () => {
     }
   }
 };
+export const getInventoryWithStockApi = async () => {
+  const url = `${urlBase}/with-stock`;
+  const token = getToken();
+  const headers = {
+    "Content-Type": "application/json",
+    "x-token": token,
+  };
+  try {
+    const response = await axios.get(url, { headers });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      return error.message;
+    }
+  }
+};
+
 export const createInventoryApi = async ({
   can_total: cantidad,
   id_producto,
