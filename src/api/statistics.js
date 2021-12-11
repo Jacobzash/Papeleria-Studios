d85e62300c0bc11b0c35668435f608d7f5d10ab3
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-11 13:59:24 -0500

    Create statistics API

---
+++ src/api/statistics.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import getToken from "../utils/getToken";
+import { apiVersion, basePath } from "./config";
+
+const urlBase = `${basePath}/${apiVersion}/statistics`;
+
+export const getStatistics = async (startDate, endDate) => {
+  console.log(startDate, endDate);
+  const url = `${urlBase}`;
+  const token = getToken();
+  const headers = {
+    "Content-Type": "application/json",
+    "x-token": token,
+  };
+  try {
+    const response = await axios.post(url, { startDate, endDate }, { headers });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      return error.message;
+    }
+  }
+};
