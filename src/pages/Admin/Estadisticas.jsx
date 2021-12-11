eda9c379235e2d436e411a377aee718430e3b58c
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-11 12:26:39 -0500

    Use FormEstadisticas component

--- src/pages/Admin/Estadisticas.jsx
+++ src/pages/Admin/Estadisticas.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { FormEstadisticas } from "../../components/Admin/Estadisticas/FormEstadisticas";
 
 export const Estadisticas = () => {
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
   return (
     <>
-      <Typography variant="h3" color="primary" align="center">
+      <Typography variant="h3" color="primary" align="center" gutterBottom>
         Modulo de Estadísticas
       </Typography>
       <Container component="div" maxWidth="lg">
-        <Typography variant="h5" color="primary" align="center">
-          Estadísticas
+        <Typography variant="h5" color="initial" align="center" gutterBottom>
+          Seleccione el intervalo de fechas para generar las estadísticas
         </Typography>
+        <FormEstadisticas dates={dates} setDates={setDates} />
       </Container>
     </>
   );
