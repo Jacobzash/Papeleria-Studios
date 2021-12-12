33ae81a59f2ca384d2b73a17450f767ddcda15a0
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-12 01:23:15 -0500

    Change Container to div

--- src/pages/Admin/Estadisticas.jsx
+++ src/pages/Admin/Estadisticas.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import { FormEstadisticas } from "../../components/Admin/Estadisticas/FormEstadisticas";
+import { ListEstadisticias } from "../../components/Admin/Estadisticas/ListEstadisticias";
 
 export const Estadisticas = () => {
   const [dates, setDates] = useState({
     startDate: new Date(),
     endDate: new Date(),
   });
+  const [statistics, setStatistics] = useState(null);
+  console.log(statistics);
   return (
     <>
       <Typography variant="h3" color="primary" align="center" gutterBottom>
         Modulo de Estadísticas
       </Typography>
-      <Container component="div" maxWidth="lg">
+      <div>
         <Typography variant="h5" color="initial" align="center" gutterBottom>
           Seleccione el intervalo de fechas para generar las estadísticas
         </Typography>
-        <FormEstadisticas dates={dates} setDates={setDates} />
-      </Container>
+        <FormEstadisticas
+          dates={dates}
+          setDates={setDates}
+          setStatistics={setStatistics}
+        />
+        {!!statistics && (
+          <ListEstadisticias
+            statistics={statistics}
+            setStatistics={setStatistics}
+          />
+        )}
+      </div>
     </>
   );
 };
