b0d3289ca402485646e25de0473db0fad32d40eb
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-15 11:59:47 -0500

    Change Info in Footer

--- src/components/Footer/InfoFooter.jsx
+++ src/components/Footer/InfoFooter.jsx
@@ -1,10 +1,14 @@
 import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import iconWp from "../../assets/iconwp.png";
 
 const useStyles = makeStyles((theme) => ({
   white: {
     color: "#FFF",
+  },
+  img: {
+    width: 48,
   },
 }));
 
@@ -16,11 +20,13 @@ export const InfoFooter = () => {
         Papelería Studios
       </Typography>
       <Typography variant="body2" className={classes.white}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-        architecto unde totam non, rerum, accusamus, eos fuga repudiandae ea
-        aspernatur quo doloribus amet minima exercitationem quam atque
-        praesentium tempore quos!
+        Tu papelería de confianza, con variedad de productos y una atención
+        personalizada. Si tiene alguna duda, puede contactarnos a través de
+        Whatsapp.
       </Typography>
+      <a rel="noreferrer" target="_blank" href="https://wa.link/hvzs6l">
+        <img src={iconWp} alt="Ícono de whatsapp" className={classes.img} />
+      </a>
     </>
   );
 };
