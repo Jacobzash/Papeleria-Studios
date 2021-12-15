9565c3467af663f1ab3e2038e44a6a08ea02d4ac
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-15 10:56:24 -0500

    Use components footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { InfoFooter } from "./InfoFooter";
+import { NavegacionFooter } from "./NavegacionFooter";
+import { UbicacionFooter } from "./UbicacionFooter";
 
 const useStyles = makeStyles((theme) => ({
   sticky: {
-    padding: theme.spacing(3, 2),
+    padding: theme.spacing(3, 10),
     marginTop: "auto",
+    background: theme.palette.primary.main,
+  },
+  white: {
+    color: "#FFF",
   },
 }));
 export const Footer = () => {
   const classes = useStyles();
   return (
     <footer className={classes.sticky}>
-      <Typography variant="h3" color="initial">
-        Footer
-      </Typography>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+        spacing={4}
+      >
+        <Grid item xs={12} md={4}>
+          <InfoFooter />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <NavegacionFooter />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <UbicacionFooter />
+        </Grid>
+      </Grid>
     </footer>
   );
 };
