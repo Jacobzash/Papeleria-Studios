623ceff9fea16818a9fa70c317621b3b777154f9
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-15 23:53:15 -0500

    Fix double item

--- src/components/Admin/Ventas/ButtonDeleteVentas.jsx
+++ src/components/Admin/Ventas/ButtonDeleteVentas.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { AdminContext } from "../../../context/AdminContext";
 
 export const ButtonDeleteVentas = ({ data, setVentas, ventas }) => {
+  const { inventory, setInventory } = useContext(AdminContext);
   const handleClick = async () => {
+    console.log(data);
+    data.setFlag(false);
+    if (inventory[0].id !== data.id) {
+      setInventory([...inventory, data]);
+    }
     setVentas(ventas.filter((venta) => venta.id !== data.id));
   };
   return (
--- src/components/Admin/Ventas/InputSearchVentas.jsx
+++ src/components/Admin/Ventas/InputSearchVentas.jsx
@@ -1,16 +1,30 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { AdminContext } from "../../../context/AdminContext";
 
 export const InputSearchVentas = ({ ventas, setVentas }) => {
-  const { inventory } = useContext(AdminContext);
+  const { inventory, setInventory, tmp, setTmp } = useContext(AdminContext);
+  useEffect(() => {
+    setTmp(inventory);
+    // eslint-disable-next-line
+  }, []);
   const [input, setInput] = useState(null);
-
+  const [flag, setFlag] = useState(false);
   const handleChange = (e, newValue) => {
+    console.log(newValue);
     setInput(newValue);
     if (newValue !== null) {
-      setVentas([...ventas, { ...newValue, cantidad: 1 }]);
+      if (inventory.length > 1) {
+        setInventory(
+          inventory.filter((inventory) => inventory.id !== newValue.id)
+        );
+        setFlag(false);
+      } else {
+        setFlag(true);
+      }
+      console.log(inventory);
+      setVentas([...ventas, { ...newValue, cantidad: 1, setFlag }]);
       setInput(null);
     }
   };
@@ -20,6 +34,7 @@ export const InputSearchVentas = ({ ventas, setVentas }) => {
       options={inventory}
       onChange={handleChange}
       value={input}
+      disabled={flag}
       getOptionLabel={(item) => item?.Producto?.nom_produc}
       renderInput={(params) => (
         <TextField {...params} label="Busca un producto" variant="outlined" />
