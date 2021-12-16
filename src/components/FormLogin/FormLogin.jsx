abe9ee789ce699c6c47ec3778fb8bec563c8af44
Marlon Gómez <marlonjulian15@gmail.com>
2021-12-16 06:53:28 -0500

    Fix warnings

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import InputAdornment from "@material-ui/core/InputAdornment";
